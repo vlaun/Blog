f5c739454e57dd699692a2d79fc4f459196f655d
Yleana Vlaun <cyvlaun4@gmail.com>
2017-02-08 09:39:23 -0600

    MathController Exercise

--- src/main/java/com/codeup/controllers/MathController.java
+++ src/main/java/com/codeup/controllers/MathController.java
@@ -1,32 +1,45 @@
-//package com.codeup.controllers;
+package com.codeup.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by ${yleanavlaun} on ${01/10/2012}.
+ */
 //
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-///**
-// * Created by ${yleanavlaun} on ${01/10/2012}.
-// */
-////
-////@GetMapping
-////@ResponseBody
-////public class MathController {
-//@Controller
-//public class HelloWorld {
-//
-//    @GetMapping("/add/3/and/4{number}")
-//    @ResponseBody
-//    public int number(@PathVariable int number) {
-//        return formatGreeting(name);
-//    }
-//
-//    private String formatGreeting(String name) {
-//        return "<h1>Hello " + name + " from Spring!!!</h1>";
-//    }
-//
-//    @RequestMapping(path = "/bye/{name}", method = RequestMethod.GET)
-//    @ResponseBody
-//    public String bye(@PathVariable String name) {
-//        return "<h1>Goodbye " + name + "! from Spring</h1>";
-//    }
-//}
+
+@Controller
+public class MathController {
+    @ResponseBody
+    @GetMapping("/add/{one}/and/{two}")
+    public int add(@PathVariable int one, @PathVariable int two) {
+        return one + two;
+
+    }
+    @ResponseBody
+    @GetMapping("/subtract/{one}/from/{two}")
+    public int subtract(@PathVariable int one, @PathVariable int two) {
+        return one - two;
+
+    }
+    @ResponseBody
+    @GetMapping("/multiply/{one}/and/{two}")
+    public int multiply(@PathVariable int one, @PathVariable int two) {
+        return one * two;
+
+    }
+    @ResponseBody
+    @GetMapping("/divide/{one}/by/{two}")
+    public double divide(@PathVariable double one, @PathVariable double two) {
+        return one / two;
+
+    }
+    @ResponseBody
+    @GetMapping("/modulus/{one}/by/{two}")
+    public int modulus(@PathVariable int one, @PathVariable int two) {
+        return one % two;
+
+    }
+
+}
 
